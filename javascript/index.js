993f47971f0f283fd4856a72aa4cb6665adc9345
Md Abu Saeed <saeedalom2024@gmail.com>
2024-09-23 23:36:23 +0600

    Error

--- javascript/index.js
+++ javascript/index.js
@@ -3,39 +3,42 @@ theme.setAttribute('data-theme',"light")
 
 
 // Donate for Flood at Noakhali 01
-let noakhaliAmount=100 ;
-let totalAmount =5500 ;
+
+
 
 document.getElementById('noakhali-onate-btn').addEventListener('click' ,function(){
     const noakhaliInput=parseFloat(document.getElementById('noakhali-input').value );
 
     if(noakhaliInput>0 && noakhaliInput!=="string" ){
-        noakhaliAmount+=noakhaliInput
-        document.getElementById('noakhali-amount').innerText= noakhaliAmount ;
-        document.getElementById('total-amount');
-        let newtotal=noakhaliAmount +totalAmount ;
-        document.getElementById('total-amount').innerText=newtotal ;
+       document.getElementById('total-amount')
+       
+       document.getElementById('noakhali-amount').innerText=newAmount
 
     }else{
-        alert("Faild to add money")
+        alert("Failed to add money")
     }
 })
 
 // Donate for Flood Relief in Feni 02
 
-let feniAmount=10 ;
+let feni = 0 ;
 
 document.getElementById('feni-onate-btn').addEventListener('click',function(){
     
     let feniInput= parseFloat(document.getElementById('feni-input').value );
 
     if(feniInput>0 && feniInput!== "string"){
-        feniAmount+=feniInput
-        let feniAmount=document.getElementById('feni-amount').innerText=feniAmount
+        feni+= feniInput 
+        document.getElementById('feni-amount').innerText=feni ;
+        document.getElementById('total-amount') ;
+        let newFaniTotal= totalAmount +feni ;
+        document.getElementById('total-amount').innerText=newFaniTotal
         
 
     }else{
-        alert("Faild to add money")
+        alert("Failed to add money")
     }
 
 })
+// Donate for Flood Relief in Feni 03
+
